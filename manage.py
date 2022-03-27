cefb9f381d5b0fe3ba6cd78e3a12c3510daacb79
zsb258 <zhuang.shengbin@u.nus.edu>
2022-03-27 12:45:53 +0800

    Fix typo

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppStore.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OPRA8.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
