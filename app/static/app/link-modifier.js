a19ade3620d6e0bc4d9d7f6104eab8de1b414a2d
thezease <100739207+thezease@users.noreply.github.com>
2022-04-03 10:18:12 +0800

    Update link-modifier.js

--- app/static/app/link-modifier.js
+++ app/static/app/link-modifier.js
@@ -71,10 +71,3 @@ function modifyViewAptLink(email) {
     return;
 }
 
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
