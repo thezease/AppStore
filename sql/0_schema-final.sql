92b78504940eae144c8f514b7c22c0bfc7da2f0a
AbdulRauf2 <70898311+AbdulRauf2@users.noreply.github.com>
2022-04-05 13:01:08 +0800

    Update 0_schema-final.sql

--- sql/0_schema-final.sql
+++ sql/0_schema-final.sql
@@ -54,7 +54,7 @@ rating INT CHECK (rating >= 1 and rating <= 5)
 );
 
 
-CREATE or REPLACE VIEW overall_ratings AS
+CREATE  VIEW overall_ratings AS
 	SELECT ap.apartment_id, CAST(AVG(r.rating) AS DECIMAL(2, 1)) AS avg_rating
 	FROM apartments ap, rentals r
 	WHERE ap.apartment_id = r.apartment_id
