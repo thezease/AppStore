d167c9ac0dfe96b96792bc45859caf9129f4deea
thezease <100739207+thezease@users.noreply.github.com>
2022-04-06 10:43:22 +0800

    Update admin.py
    Trying to catch ratings error

--- app/admin.py
+++ app/admin.py
@@ -803,6 +803,26 @@ def rentals_edit(request, id):
                         status = f'Violated constraint: There is already a prior booking.'
                         result_dict['status'] = status
                         
+                    elif 'unable to rate before staying' in e_msg:
+                        with connection.cursor as cursor:
+                            cursor.execute(
+                                """
+                                UPDATE rentals
+                                SET apartment_id = %s, 
+                                check_in = %s, 
+                                check_out = %s, 
+                                guest = %s,
+                                WHERE rental_id = %s;""",
+                                [
+                                request.POST['apartment_id'],
+                                request.POST['check_in'],
+                                request.POST['check_out'],
+                                request.POST['guest'],
+                                id
+                                ]
+                                )
+                        status = 'Unable to rate before staying. Details updated successfully'
+                        result_dict['status'] = status
                     else:
                         status = f'Violated constraint: Wrong format. Please follow the required format.'
                         result_dict['status'] = status
