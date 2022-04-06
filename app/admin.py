c1d282db8d2668cc24d3138cbdf218defed99131
AbdulRauf2 <70898311+AbdulRauf2@users.noreply.github.com>
2022-04-06 10:19:15 +0800

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -800,12 +800,14 @@ def rentals_edit(request, id):
 
                 except DatabaseError as err:
                     e_msg = str(err.__cause__)
-                    constraint = re.findall(r'(?<=\")[A-Za-z\_]*(?=\")', e_msg)[-1]
-                    if constraint == 'datestyle':
-                        status = f'Violated constraint: {constraint}. Invalid date.Please enter a valid date.'
+                    
+                    if 'prior booking' in e_msg:
+                        status = f'Violated constraint: There is already a prior booking.'
                         result_dict['status'] = status
+                        
                     else:
-                        status = f'Violated constraint: {constraint}. There is already a prior booking.'
+                        constraint = re.findall(r'(?<=\")[A-Za-z\_]*(?=\")', e_msg)[-1]
+                        status = f'Violated constraint: {constraint}. Invalid date.Please enter a valid date.'
                         result_dict['status'] = status
                     return render(request, "app/admin_rentals_edit.html", result_dict)
                     
